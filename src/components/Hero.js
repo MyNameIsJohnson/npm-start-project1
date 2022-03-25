1eee09ba762552d8b651ab403f33345a913ffb8a
johnsonthieu <johnsonle2929@gmail.com>
2022-03-24 17:07:37 -0700

    updates header on scroll

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='hero' style={{ backgroundImage: "url('images/homepage.png')" }}>
+    <div id="hero" className='' style={{ backgroundImage: "url('images/homepage.png')" }}>
       <div className='welcome'>
         <h1>Changing Lives Through Tech</h1>
         <p>The Last Mile prepares incarcerated individuals for successful reentry through business and technology training.</p>
